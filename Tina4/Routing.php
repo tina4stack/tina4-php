742d65e5193dc8f75bbf0bd8d01b0df2a3cf0340
Andre van Zuydam <andrevanzuydam@gmail.com>
2019-08-27 23:07:56 +0200

    Stable release
    Added PATCH to access control origins

--- Tina4/Routing.php
+++ Tina4/Routing.php
@@ -31,7 +31,7 @@ class Routing
             if (key_exists("HTTP_ORIGIN", $_SERVER) ) {
                 header('Access-Control-Allow-Origin: ' . $_SERVER["HTTP_ORIGIN"]);
             }
-            header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
+            header('Access-Control-Allow-Methods: GET, PUT, POST, PATCH, DELETE, OPTIONS');
             header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
             header('Access-Control-Allow-Credentials: true');
         }
