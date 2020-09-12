4cd584e0b86defbf615ba15384b636bc5fc5f92a
Andre van Zuydam <andrevanzuydam@gmail.com>
2020-09-12 12:12:50 +0200

    Code optimizations

--- Tina4/Data.php
+++ Tina4/Data.php
@@ -14,23 +14,11 @@ class Data
     {
         //Check if we have a database connection declared as global , add it to the data class
         foreach ($GLOBALS as $dbName => $GLOBAL) {
-
             if (!empty($GLOBAL)) {
-                if ($dbName[0] == "_" || $dbName === "cache") continue;
+                if ($dbName[0] == "_" || $dbName === "cache" || $dbName == "auth") continue;
                 if (is_object($GLOBAL) && in_array(get_class($GLOBAL) , TINA4_DATABASE_TYPES) ) {
                     DebugLog::message("Adding {$dbName}");
                     $this->{$dbName} = $GLOBAL;
-                }
-            }
-        }
-
-        //Assign database connections directly based on the variables
-        if (!empty($DBA) && !is_array($DBA)) {
-            $this->DBA = $DBA;
-        } else {
-            if (is_array($DBA)) {
-                foreach ($DBA as $dbName => $dbHandle) {
-                    $this->{$dbName} = $dbHandle;
                 }
             }
         }
