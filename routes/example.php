87717060c0003160a5a83e429f8b54ea8fe0120b
Andre van Zuydam <andrevanzuydam@gmail.com>
2020-12-21 12:44:07 +0200

    Added example of a route

---
+++ routes/example.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Example of a get route - access http://localhost:8080/hello-world
+ * Assuming you have run php -S localhost:8080 index.php
+ */
+\Tina4\Get::add("/hello-world", function(\Tina4\Response $response){
+
+    return $response ("Hello World!");
+});
+
+
+/**
+ * Example of a get route with inline params - access http://localhost:8080/hello-world/test
+ * Assuming you have run php -S localhost:8080 index.php
+ */
+\Tina4\Get::add("/hello-world/{name}", function($name, \Tina4\Response $response){
+
+    return $response ("Hello World {$name}!");
+});
