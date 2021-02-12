73253117d5d6eb6da9353f408895d42e5d213b7f
Andre van Zuydam <andrevanzuydam@gmail.com>
2021-02-12 09:08:24 +0200

    Fix for Firebird error handling

--- index.php
+++ index.php
@@ -26,8 +26,8 @@ global $DBA;
 //$DBA = (new \Tina4\DataFirebird(FIREBIRD_DATABASE,FIREBIRD_USERNAME,FIREBIRD_PASSWORD));
 //$DBA =  new \Tina4\DataSQLite3("auth.db");
 
-global $DBA;
-$DBA = (new \Tina4\DataFirebird(DB_SERVER,DB_USERNAME,DB_PASSWORD)); //database connection
+//global $DBA;
+//$DBA = (new \Tina4\DataFirebird(DB_SERVER,DB_USERNAME,DB_PASSWORD)); //database connection
 
 //For mysql - docker-compose up in the relevant docker folder before running
 //$DBA = new \Tina4\DataMySQL("127.0.0.1/5508:tina4", "tina4", "Password1234", "d/m/Y");
