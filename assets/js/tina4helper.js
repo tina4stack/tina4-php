645573a5ff9b333c206b0ef72badacb5b647bd94
Andre van Zuydam <andrevanzuydam@gmail.com>
2021-03-04 12:54:50 +0200

    Fixes for iterate directory

--- assets/js/tina4helper.js
+++ assets/js/tina4helper.js
@@ -104,7 +104,31 @@ function getCookie(name) {
     return null;
 }
 
+//https://stackoverflow.com/questions/4068373/center-a-popup-window-on-screen
+const popupCenter = ({url, title, w, h}) => {
+    // Fixes dual-screen position                             Most browsers      Firefox
+    const dualScreenLeft = window.screenLeft !==  undefined ? window.screenLeft : window.screenX;
+    const dualScreenTop = window.screenTop !==  undefined   ? window.screenTop  : window.screenY;
 
+    const width = window.innerWidth ? window.innerWidth : document.documentElement.clientWidth ? document.documentElement.clientWidth : screen.width;
+    const height = window.innerHeight ? window.innerHeight : document.documentElement.clientHeight ? document.documentElement.clientHeight : screen.height;
+
+    const systemZoom = width / window.screen.availWidth;
+    const left = (width - w) / 2 / systemZoom + dualScreenLeft
+    const top = (height - h) / 2 / systemZoom + dualScreenTop
+    const newWindow = window.open(url, title,
+        `
+      directories=no,toolbar=no,location=no,status=no,menubar=no,scrollbars=no,resizable=no,
+      width=${w / systemZoom}, 
+      height=${h / systemZoom}, 
+      top=${top}, 
+      left=${left}
+      `
+    )
+
+    if (window.focus) newWindow.focus();
+    return newWindow;
+}
 /*
  * bootstrap-tagsinput v0.6.1 by Tim Schlechter
  *
