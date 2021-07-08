a023737533120a580b4cbfc7e90b23d42cd8ff9e
Andre van Zuydam <andrevanzuydam@gmail.com>
2021-07-08 18:31:40 +0200

    Updated Tina4 Php Env

--- index.php
+++ index.php
@@ -1,6 +1,13 @@
 <?php
 require_once "vendor/autoload.php";
 
-$config = new \Tina4\Config();
+$config = new \Tina4\Config(function (\Tina4\Config $config) {
+
+    $config->addTwigGlobal("HELLO", "ME");
+    $config->addTwigFunction("getUsers", function($myvar) {
+        return $myvar;
+    });
+
+});
 
 echo new \Tina4\Tina4Php($config);
