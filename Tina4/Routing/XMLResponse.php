46746e93991f3d19e84033eed7837f7a4e309700
Andre van Zuydam <andrevanzuydam@gmail.com>
2021-11-02 22:44:10 +0200

    Added updates for the XMLResponse

--- Tina4/Routing/XMLResponse.php
+++ Tina4/Routing/XMLResponse.php
@@ -9,6 +9,31 @@ class XMLResponse
     //XML Serialize taken from Stack Overflow
     //https://stackoverflow.com/questions/137021/php-object-as-xml-document
 
+    /**
+     * Taken from stack over flow
+     * https://stackoverflow.com/questions/4554233/how-check-if-a-string-is-a-valid-xml-with-out-displaying-a-warning-in-php
+     * @param $content
+     * @return bool
+     */
+    public static function isValidXml($content): bool
+    {
+        $content = trim($content);
+        if (empty($content)) {
+            return false;
+        }
+        //html go to hell!
+        if (stripos($content, '<!DOCTYPE html>') !== false) {
+            return false;
+        }
+
+        libxml_use_internal_errors(true);
+        simplexml_load_string($content);
+        $errors = libxml_get_errors();
+        libxml_clear_errors();
+
+        return empty($errors);
+    }
+
 
     /**
      * Initializes the XML header
@@ -18,7 +43,10 @@ class XMLResponse
      */
     public static function generateValidXmlFromArray($array, $nodeName = 'node'): string
     {
-        $xml  = self::generateXmlFromArray($array, $nodeName);
+        if (!self::isValidXml($array) && (is_object($array) || is_array($array))) {
+            $xml  = self::generateXmlFromArray($array, $nodeName);
+        }
+
         $xml  = str_replace('<node>', '', $xml);
         $xml  = str_replace('</node>', '', $xml);
 
