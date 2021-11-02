e6b10588c9d775de1623e63a40fe0cfa1c6ee981
Andre van Zuydam <andrevanzuydam@gmail.com>
2021-11-02 22:31:35 +0200

    Added updates for the XMLResponse

--- Tina4/Routing/XMLResponse.php
+++ Tina4/Routing/XMLResponse.php
@@ -13,17 +13,16 @@ class XMLResponse
     /**
      * Initializes the XML header
      * @param array $array
-     * @param string $node_block
      * @param string $node_name
      * @return string
      */
-    public static function generateValidXmlFromArray($array, $node_block = 'nodes', $node_name = 'node'): string
+    public static function generateValidXmlFromArray($array, $nodeName = 'node'): string
     {
-        $xml = '<?xml version="1.0" encoding="UTF-8" ?>';
-        $xml .= '<' . $node_block . '>';
-        $xml .= self::generateXmlFromArray($array, $node_name);
-        $xml .= '</' . $node_block . '>';
-        return $xml;
+        $xml  = self::generateXmlFromArray($array, $nodeName);
+        $xml  = str_replace('<node>', '', $xml);
+        $xml  = str_replace('</node>', '', $xml);
+
+        return '<?xml version="1.0" encoding="UTF-8" ?>'.$xml;
     }
 
     /**
@@ -32,19 +31,21 @@ class XMLResponse
      * @param $node_name
      * @return string
      */
-    private static function generateXmlFromArray($array, $node_name): string
+    private static function generateXmlFromArray($array, $nodeName): string
     {
         $xml = '';
         if (is_array($array) || is_object($array)) {
             foreach ($array as $key => $value) {
                 if (is_numeric($key)) {
-                    $key = $node_name;
+                    $key = $nodeName;
                 }
-                $xml .= '<' . $key . '>' . self::generateXmlFromArray($value, $node_name) . '</' . $key . '>';
+                $xml .= '<' . $key . '>' . self::generateXmlFromArray($value, $nodeName) . '</' . $key . '>';
             }
         } else {
             $xml = htmlspecialchars($array, ENT_QUOTES);
         }
         return $xml;
     }
+
+
 }
