385b15772171d1394be9bc9614b5052f06eb3ef8
Andre van Zuydam <andrevanzuydam@gmail.com>
2023-07-05 11:56:38 +0200

    Fix status on deployments

--- Tina4/Deploy/GitDeploy.php
+++ Tina4/Deploy/GitDeploy.php
@@ -35,7 +35,7 @@ class GitDeploy
 
         if (!empty($_ENV["GIT_REPOSITORY"])) {
             //Make sure branch matches the branch specified in the GIT_BRANCH
-            if (!isset($request->data->workflow_run) && $request->data->action === "completed" && $request->data->workflow_run->status !== "completed")
+            if (!isset($request->data->workflow_run) && $request->data->action !== "completed" && $request->data->workflow_run->status !== "completed")
             {
                 return false;
             }
