d1987c80579a2d6c6b0babd5dfc8eac2f1d94ebe
Andre van Zuydam <andrevanzuydam@gmail.com>
2023-08-01 11:41:00 +0200

    Added back compile scss for documentation

--- index.php
+++ index.php
@@ -11,4 +11,9 @@ $config = new \Tina4\Config(static function (\Tina4\Config $config){
 //$test = (new \Tina4\Slack())->getChannels();
 //print_r ($test);
 
+//Hack to build css for documentation
+$scss = new ScssPhp\ScssPhp\Compiler();
+$scssDefault = $scss->compileString(file_get_contents("./src/templates/documentation/default.scss"))->getCss();
+file_put_contents("./src/templates/documentation/default.css", $scssDefault);
+
 echo new \Tina4\Tina4Php($config);
